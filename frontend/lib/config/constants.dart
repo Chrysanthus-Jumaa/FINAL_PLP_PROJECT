399391a417bd95ac229be660a66fae7584795f46
Chrysanthus Jumaa <jumaachrysanthus@gmail.com>
2025-11-19 14:48:26 +0300

    Application is working normally. Prepping for live deployment

--- frontend/lib/config/constants.dart
+++ frontend/lib/config/constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   // API Configuration
-  static const String baseUrl = 'http://10.0.2.2:8000/api'; // Android emulator
+  static const String baseUrl = 'http://localhost:8000/api'; // Android emulator
   // Use 'http://localhost:8000/api' for web
   // Use 'http://YOUR_COMPUTER_IP:8000/api' for physical device
   
